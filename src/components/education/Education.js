7baa308a354902c5ec14e1695e78e047600ee168
surbhigoel555 <surbhigoel555@gmail.com>
2024-09-09 15:38:59 +0530

    Education added

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import TimelineItem from "../common/TimelineItem";
+import "../common/TimelineItem.css";
 
 export default function Education() {
+  const eduList = [
+    {
+      text: "M.M. University (Mullana)",
+      text2:
+        "Subjects: Data Structure, Discrete mathematics, Internet Fundamentals, Computer Graphics etc",
+      date: "2011-14",
+      category: {
+        tag: "B.Tech CSE",
+        color: "#018f69",
+      }
+    },
+    {
+      text: "M.M. University (Mullana)",
+      text2:
+        "Subjects: Object-Oriented Programming using C++, C, Java, Internet Fundamentals, Discrete Structure etc",
+      date: "2008-11",
+      category: {
+        tag: "Diploma CSE",
+        color: "#018f69",
+      }
+    },
+  ];
+
   return (
-    <div>
-      Education
-    </div>
-  )
+    <>
+      {eduList.length > 0 && (
+        <div className="timeline-container">
+          {eduList.map((data, idx) => (
+            <TimelineItem data={data} key={idx} />
+          ))}
+        </div>
+      )}
+      ;
+    </>
+  );
 }
